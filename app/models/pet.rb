800441ddde33ac90f419d40dde2637e6d1430d3a
Takahashi-Ke <ketakahashi6@gmai.com>
2020-12-17 12:55:24 +0000

    [Update]enum

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,8 +1,14 @@
 class Pet < ApplicationRecord
   
   belongs_to :owner
+  has_many :pet_personalities, dependent: :destroy
   
   attachment :image
   validates :name, presence: true
+  
+  enum gender: {
+    男の子: 1,
+    女の子: 2
+  }
   
 end
