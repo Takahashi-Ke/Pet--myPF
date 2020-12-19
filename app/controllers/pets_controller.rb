559f227b1af57d4cca88a7b1d01a9d2638e7fbf4
Takahashi-Ke <ketakahashi6@gmai.com>
2020-12-19 05:26:07 +0000

    [Add]view-pet

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,13 @@
 class PetsController < ApplicationController
+
+  def show
+    @pet = Pet.find_by(id: current_owner.pet)
+    @personalities = @pet.pet_personalities
+  end
+
+  def edit
+    @pet = Pet.find(params[:id])
+    @personalities = @pet.pet_personalities
+  end
+
 end
