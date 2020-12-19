81e5255f2cdec5ce09ce557bba818e60a3bdc4c1
Takahashi-Ke <ketakahashi6@gmai.com>
2020-12-19 12:46:32 +0000

    [Update]owner-pet-edit

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -10,4 +10,16 @@ class PetsController < ApplicationController
     @personalities = @pet.pet_personalities
   end
 
+  def update
+    pet = Pet.find(params[:id])
+    current_owner.update(owner_pet_params)
+    redirect_to pet_path(pet)
+  end
+
+  private
+  def owner_pet_params
+    params.require(:owner).permit(:name, :image, pet_attributes: [:id, :name, :image, :birthday, :gender, :type, :introduction, :_destroy,
+                                      pet_personalities_attributes: [:id, :personality, :_destroy]])
+  end
+
 end
