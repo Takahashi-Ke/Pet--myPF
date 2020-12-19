eddb5b71584a9c1bb0a8d4c597e93769501cecdc
Takahashi-Ke <ketakahashi6@gmai.com>
2020-12-19 02:12:24 +0000

    [Update]log_in

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -2,7 +2,7 @@ class Owner < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable
   has_many :pets, dependent: :destroy
   accepts_nested_attributes_for :pets, allow_destroy: true
 
