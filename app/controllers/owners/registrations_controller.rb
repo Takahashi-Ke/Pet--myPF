1689a2cb9582f9bc3a91f56a79f5d8c427f24996
Takahashi-Ke <ketakahashi6@gmai.com>
2020-12-26 09:41:49 +0000

    [Update]pet-profile

--- app/controllers/owners/registrations_controller.rb
+++ app/controllers/owners/registrations_controller.rb
@@ -14,9 +14,6 @@ class Owners::RegistrationsController < Devise::RegistrationsController
   # POST /resource
   def create
     super
-    owner = Owner.new(configure_sign_up_params)
-    owner.pet.gender.to_i
-    owner.save
   end
 
   # GET /resource/edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :owners
+  devise_for :owner, controllers: {
+    registrations: 'owners/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#top'
   resources :owners, only: [:index] do
